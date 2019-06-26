c28555fcfc2aed88250fb002c6853aacfc5453d6
Spenser Truex <spensertruexonline@gmail.com>
2019-06-26 16:06:15 -0700

    Add keybindings.

--- keybindings.el
+++ keybindings.el
@@ -1,13 +1,13 @@
 (spacemacs/declare-prefix "tt" "timeclock")
 (spacemacs/set-leader-keys
-  "tti" 'timeclock-in
-  "tto" 'timeclock-out
-  "ttc" 'timeclock-change
-  "ttr" 'timeclock-reread-log
-  "ttu" 'timeclock-update-mode-line
-  "ttw" 'timeclock-when-to-leave-string
-  "tts" 'timeclock-status-string
-  "ttm" 'timeclock-modeline-display
-  "ttg" 'timeclock-workday-remaining-string
-  "tte" 'timeclock-workday-elapsed-string
-  "ttl" 'timeclock-when-to-leave-string)
+   "tti"          'timeclock-in
+   "tto"          'timeclock-out
+   "ttc"          'timeclock-change
+   "ttr"          'timeclock-reread-log
+   "ttu"          'timeclock-update-mode-line
+   "ttw"          'timeclock-when-to-leave-string
+   "tts"          'timeclock-status-string
+   "ttm"          'timeclock-modeline-display
+   "ttg"          'timeclock-workday-remaining-string
+   "tte"          'timeclock-workday-elapsed-string
+   "ttl"          'timeclock-when-to-leave-string)
