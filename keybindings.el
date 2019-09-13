1c75aa6bee376cb44f0d4b3111d104e914fd872d
Spenser Truex <spensertruexonline@gmail.com>
2019-09-13 16:16:47 -0700

    Revert "Added timelog selector function to separate public GSP stuff."
    This reverts commit 5ea5a454993dad0f0e37363e480bc1d576202fd8.

--- keybindings.el
+++ keybindings.el
@@ -1,31 +1,7 @@
-(defvar timeclock-log-dirs '(("g" "~/.emacs.d/private/timelogs/gsp-log")
-                             ("p" "~/.emacs.d/private/timelogs/private-log")))
-(defvar timeclock-in-dir timeclock-file)
-(defvar timeclock-temp-file timeclock-file)
-(defun timeclock-in-selector ()
-  (interactive)
-  (labels ((inner
-            (warn-next)
-            (let ((selection (downcase (read-from-minibuffer
-                                        (if warn-next
-                                            "INVALID; [G]sp or [P]rivate timelog? [p/g]"
-                                          "[G]sp or [P]rivate timelog? [p/g]")))))
-              (if (member selection (mapcar #'car timeclock-log-dirs))
-                  (progn (setf timeclock-temp-file timeclock-file)
-                         (setf timeclock-file (cadr (assoc selection timeclock-log-dirs)))
-                         (timeclock-in 0 nil t))
-                (inner t)))))
-    (inner nil)))
-(defun timeclock-out-selector ()
-  (interactive)
-  ;; KLUDGE: This function should not need to exist. timeclock-in-selector is
-  ;; the only special one that should be needed.
-  (timeclock-out 0 nil t)
-  (setf timeclock-file timeclock-temp-file))
 (spacemacs/declare-prefix "tt" "timeclock")
 (spacemacs/set-leader-keys
-   "tti"          'timeclock-in-selector
-   "tto"          'timeclock-out-selector
+   "tti"          'timeclock-in
+   "tto"          'timeclock-out
    "ttc"          'timeclock-change
    "ttr"          'timeclock-reread-log
    "ttu"          'timeclock-update-mode-line
